8b380c6d7668800bcc1788a1343f748e7f5b1f1a
Jake-Don <110348791+JakeDon005@users.noreply.github.com>
2023-02-07 22:05:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,30 @@
 var cacheStorageKey = 'minimal-pwa-1'
 
 var cacheList = [
-    "index.html",
-    "./modules/vue.global.js",
-    "./modules/ion.rangeSlider.css",
-    "./modules/ion.rangeSlider.min.js",
-    "./modules/jquery-3.6.3.min.js",
-    "./js-css/style.css",
-    "./js-css/program.js",
-    "./scr/pitcherLogo.ico"
+  "index.html",
+  "manifest.json",
+  "./modules/vue.global.js",
+  "./modules/ion.rangeSlider.css",
+  "./modules/ion.rangeSlider.min.js",
+  "./modules/jquery-3.6.3.min.js",
+  "./js-css/style.css",
+  "./js-css/program.js",
+  "./scr/pitcherLogo.ico"
 ]
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheStorageKey)
-            .then(cache => cache.addAll(cacheList))
-            .then(() => self.skipWaiting())
-    )
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+      .then(cache => cache.addAll(cacheList))
+      .then(() => self.skipWaiting())
+  )
 })
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        if (response != null) {
-          return response
-        }else{
-            return fetch(e.request.url)  .catch(error => {
-                console.log('There has been a problem with your fetch operation:', error);})
-        }
-
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    fetch(e.request.url).catch(error => {
+      console.log('从网络请求失败', error);
+      return caches.match(e.request).catch(error =>{
+        console.error('错得离谱',error)
       })
-    )
-  })
+    })
+  )
+})
